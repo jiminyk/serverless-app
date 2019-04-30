a263b908feded487e3f47e93c430224cbe3441fb
Christian Gomez <jiminiiik@gmail.com>
2019-04-30 14:23:24 -0400

    created a jasmine spec to test a functionality

---
+++ public/app.js
@@ -0,0 +1,9 @@
+// create the namespace learnjs
+'use strict';
+
+var learnjs = {};
+
+learnjs.showView = function(hash){
+    var problemView = $('<div class="problem-view">').text('Coming soon!');
+    $('.view-container').empty().append(problemView);
+}
